ec1faa75623f64cbe5d68b454f5c370709328a76
Skim0082 <Skim0082@conestogac.on.ca>
2015-11-05 13:52:18 -0500

    Worked by SungJoe
    Worked by JOE

--- Lab5_Intergration_Testing/Lab5_Intergration_Testing/Form1.cs
+++ Lab5_Intergration_Testing/Lab5_Intergration_Testing/Form1.cs
@@ -19,7 +19,31 @@ namespace Lab5_Intergration_Testing
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int yearOfBirth = int.Parse(textBox1.Text);
+            int age = CheckAge(yearOfBirth);
+            if (age > 19)
+            {
+                label2.Text = "Your age is " + age +
+                    ".\n Your are now allowed to\n drink alcohol in South Korea";
+            }
+            else
+            {
+                label2.Text = "Your age is " + age +
+                    ".\n Your are allowed to\n drink alcohol in South Korea";
+            }
+        }
 
+        public static int CheckAge(int yearOfBirth)
+        {            
+            int currentYear = DateTime.Now.Year;
+            return currentYear - yearOfBirth;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Input your year of birth";
+            button1.Text = "Check your age";
+            label2.Text = "Your age is";
         }
     }
 }
---
+++ Lab5_Intergration_Testing/Lab5_Intergration_TestingTests/Form1Tests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab5_Intergration_Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Intergration_Testing.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        /*
+         Test Identifier: 1.1
+         * Test Description: Testing age by 2000
+         * Input Data: 2000
+         * Actions: check the age from input value
+         * Expected Output: 15
+         * OBserved Output: will be reported
+         * Failed Action: Alert the user that their input failed to check 2000 
+         */
+        [TestMethod()]
+        public void CheckAgeTest2000()
+        {
+            int yearOfBirth = 2000;
+            int result = Lab5_Intergration_Testing.Form1.CheckAge(yearOfBirth);
+            Assert.AreEqual(15, result);
+        }
+
+        /*
+         Test Identifier: 1.2
+         * Test Description: Testing age by 1990
+         * Input Data: 1990
+         * Actions: check the age from input value
+         * Expected Output: 25
+         * OBserved Output: will be reported
+         * Failed Action: Alert the user that their input failed to check 1990 
+         */
+        [TestMethod()]
+        public void CheckAgeTest1990()
+        {
+            int yearOfBirth = 1990;
+            int result = Lab5_Intergration_Testing.Form1.CheckAge(yearOfBirth);
+            Assert.AreEqual(25, result);
+        }
+
+        /*
+         Test Identifier: 1.3
+         * Test Description: Testing age by 1980
+         * Input Data: 1980
+         * Actions: check the age from input value
+         * Expected Output: 35
+         * OBserved Output: will be reported
+         * Failed Action: Alert the user that their input failed to check 1980 
+         */
+        [TestMethod()]
+        public void CheckAgeTest1980()
+        {
+            int yearOfBirth = 1980;
+            int result = Lab5_Intergration_Testing.Form1.CheckAge(yearOfBirth);
+            Assert.AreEqual(35, result);
+        }
+    }
+}
